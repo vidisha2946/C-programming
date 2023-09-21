596a5f900eee9ec0e073da91e136f80038abb9e7
Vidisha <140824909+vidisha2946@users.noreply.github.com>
2023-09-21 19:49:41 +0530

    Add files via upload

---
+++ lab 10/perfect.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+   int number,i,result=0;
+   printf("enter the number:");
+   scanf("%d",&number);
+   for(i=1;i<=number;i++){
+      if(number%i==0)
+         result=result+i;
+   }
+   if(result==2*number)
+      printf("perfect number");
+   else
+      printf("not perfect number");
+}
