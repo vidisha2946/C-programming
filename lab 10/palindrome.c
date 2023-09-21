382a58fe92aafd86274211cfeac7a0ea39da56ca
Vidisha <140824909+vidisha2946@users.noreply.github.com>
2023-09-21 19:55:48 +0530

    Add files via upload

---
+++ lab 10/palindrome.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int number, originalNumber, reversedNumber = 0;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    originalNumber = number; 
+    while (number != 0) {
+        int digit = number % 10; 
+        reversedNumber = reversedNumber * 10 + digit; 
+        number /= 10; 
+    }
+    if (originalNumber == reversedNumber) {
+        printf("%d is a palindrome.\n", originalNumber);
+    } else {
+        printf("%d is not a palindrome.\n", originalNumber);
+    }
+}
