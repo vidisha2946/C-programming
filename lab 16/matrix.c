e7687a0862bb4ec7f455c29bb39b0cc240aedbc4
Vidisha <140824909+vidisha2946@users.noreply.github.com>
2023-10-19 18:49:27 +0530

    Add files via upload

---
+++ lab 16/matrix.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int data[3][3],i,j;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("enter array elements: ");
+            scanf("%d",&data[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d ",data[i][j]);
+        }
+        printf("\n");
+    }
+}
