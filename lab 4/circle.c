e996b8203e30551f0d5e9062bedfc82aea9d91e3
Vidisha <140824909+vidisha2946@users.noreply.github.com>
2023-11-23 11:27:13 +0530

    Update circle.c

--- lab 4/circle.c
+++ lab 4/circle.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-int main() {
+void main() {
     float radius;
     printf("Enter the radius of the circle: ");
     scanf("%f", &radius);
     float area = 3.14159265359 * radius * radius;
     printf("%f,%f\n", radius, area);
-    return 0;
 }
 
